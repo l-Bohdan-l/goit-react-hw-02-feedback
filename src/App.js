bb3e0c02d2ac729c73999e691186a7d1390e0d96
l-Bohdan-l <bpapushoj@gmail.com>
2021-12-07 00:05:11 +0200

    добавляет базовые файлы и счетчик отзывов

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.scss';
-import { Profile } from './components/Profile/Profile';
-import userData from './components/Profile/user.json';
-import { Statistics } from './components/Statistics/Statistics';
-import data from './components/Statistics/data.json';
-import { FriendsList } from './components/FriendsList/FriendsList';
-import friends from './components/FriendsList/friends.json';
-import { Transactions } from './components/Transaction-history/Transactions';
-import items from './components/Transaction-history/transactions.json';
+import { Feedbcak } from './components/Feedback/Feedback';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Profile user={userData} />
-        <Statistics title="Upload stats" stats={data} />
-        <FriendsList friends={friends} />
-        <Transactions items={items} />
+        <Feedbcak />
       </header>
     </div>
   );
